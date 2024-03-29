f9fe66934ec0c25ef92a6ea9393268190fb74b31
SalasaNopa <160710612+SalasaNopa@users.noreply.github.com>
2024-03-29 09:56:49 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\GajiController;
+
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\PresensiController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home', ['status' => 'Home']);
+});
+
+Route::get('/gaji', [GajiController::class, 'index']);
+
+Route::get('/masterjabatan', [JabatanController::class, 'index']);
+
+Route::get('/presensi', [PresensiController::class, 'index']);
